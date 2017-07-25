e19d4a46fb41fa21c9841d3df5aac078cf54a7ef
Ricardo Rojas <reddrum7@gmail.com>
2017-07-25 17:40:30 -0500

    Add filtering input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AttendeeService } from './attendee.service';
-import { Component, OnInit } from '@angular/core';
 import { Attendee } from './attendee.model';
+import { DatatableComponent } from '@swimlane/ngx-datatable/release';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,19 @@ import { Attendee } from './attendee.model';
 export class AppComponent implements OnInit {
   title = 'Leaderboard';
   rows: Attendee[] = [];
+  temp: Attendee[] = [];
 
+  columns = [
+    { prop: 'name' },
+    { prop: 'photo' },
+    { prop: 'homeworks' },
+    { prop: 'average_grade' },
+    { prop: 'delivered_homeworks' },
+    { prop: 'missing_homeworks' },
+    { prop: 'updated_at' }
+  ];
+
+  @ViewChild(DatatableComponent) table: DatatableComponent;
   constructor(private attendeeService: AttendeeService) {}
 
   ngOnInit() {
@@ -21,12 +34,30 @@ export class AppComponent implements OnInit {
   getAttendees() {
     this.attendeeService.getAttendees()
       .subscribe(rows => {
+        // cache out list
+        this.temp = [...rows];
+        // push our inital complete list
         this.rows = rows;
+
         this.averageGrade();
       });
   }
 
   averageGrade() {
     this.attendeeService.calculateAverageGrade(this.rows);
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
   }
 }
